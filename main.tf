62e6faca3aab2db7c20d2fa973399a13da3fdf65
Akshay <akshay@example.com>
2026-04-28 18:47:28 +0530

    Update Terraform project

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "ap-south-1"
 }
 
 terraform {
   backend "s3" {
-    bucket         = "s3-bucketnew-task"   
+    bucket         = "s3-bucketnew-task"
     key            = "terraform.tfstate"
-    region         = "eu-north-1"
+    region         = "ap-south-1"
     dynamodb_table = "Dynamo-DB"
   }
 }
@@ -17,7 +17,7 @@ module "vpc" {
   vpc_cidr        = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  azs             = ["eu-north-1a", "eu-north-1b"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
 }
 
 module "alb" {
@@ -33,11 +33,9 @@ module "ec2" {
   vpc_id           = module.vpc.vpc_id
   private_subnets  = module.vpc.private_subnets
   target_group_arn = module.alb.target_group_arn
-  alb_sg           = module.alb.alb_sg   
+  alb_sg           = module.alb.alb_sg
 
   ami              = var.ami
   instance_type    = var.instance_type
   desired_capacity = var.desired_capacity
 }
-
-
